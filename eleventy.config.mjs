6a0faa9b631ca937e6a6173c5375a5204b98dcd0
uxtbd <dev@kthread.dev>
2026-07-17 01:39:41 -0300

    test 6

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,7 @@
 import markdownIt from "markdown-it";
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import readingTime from "eleventy-plugin-reading-time";
 
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory("src");
@@ -7,17 +9,37 @@ export default function (eleventyConfig) {
   eleventyConfig.setIncludesDirectory("_includes");
   eleventyConfig.setLayoutsDirectory("_layouts");
 
-  eleventyConfig.addPassthroughCopy("src/assets/*.css"); // CSS files
+  eleventyConfig.addPassthroughCopy("src/**.css"); // CSS files
   eleventyConfig.addPassthroughCopy("src/scripts/*.js"); // JavaScript files
+  eleventyConfig.addPassthroughCopy("src/assets/**/*.{png,jpg,mp4}"); // Media files
 
   let options = {
     html: true,
     breaks: true,
     linkify: true,
   };
-
-  eleventyConfig.addPlugin(syntaxHighlight);
-
+  
   eleventyConfig.setLibrary("md", markdownIt(options));
   eleventyConfig.amendLibrary("md", (mdLib) => mdLib.enable("code"));
+
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/rss.xml",
+		collection: {
+			name: "blogs", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "ux | blogs",
+			subtitle: "ramblings",
+			base: "https://kthread.dev",
+			author: {
+				name: "uxtbd",
+				email: "dev@kthread.dev",
+			}
+		}
+	});
+	eleventyConfig.addPlugin(readingTime);
 }
--- src/index.md
+++ src/index.md
@@ -6,4 +6,15 @@ description: "software engineer"
 
 # Welcome!
 
-## 21-year-old software engineer from south america
+21-year-old software engineer from south america, I am quite a pleco fan, I love plecos, I love cats as well.
+
+```c
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello syntax highlighting\n");
+  return 0;
+}
+```
+
+# Projects
